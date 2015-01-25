17fc803599b1913b1617de650668109ffac33a7f
Jozef Wagner <jozef.wagner@gmail.com>
2015-01-25 21:46:55 +0100

    deploy config

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject org.dunaj/tools.analyzer "0.6.5-dunaj-SNAPSHOT"
+(defproject org.dunaj/tools.analyzer "0.6.5-dunaj_pre1"
   :description "An analyzer for Clojure code, written in Clojure and producing AST in EDN."
-  :url "https://github.com/clojure/tools.analyzer"
+  :url "https://github.com/dunaj/tools.analyzer"
+  :scm {:name "git" :url "https://github.com/dunaj-project/tools.analyzer"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :signing {:gpg-key "6A72CBE2"}
+  :deploy-repositories [["clojars" {:creds :gpg}]]
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :dependencies [#_[org.clojure/clojure "1.7.0-alpha4"]
